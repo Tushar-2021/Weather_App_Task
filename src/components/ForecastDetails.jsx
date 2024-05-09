8cd21a90a7d36f066a654c47632addb561d48f4c
Tushar-2021 <tusharma2002@outlook.com>
2024-05-09 21:00:08 +0530

    Deployment error2

--- src/components/ForecastDetails.jsx
+++ src/components/ForecastDetails.jsx
@@ -1,43 +1,44 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faCloudShowersHeavy, faCloud } from '@fortawesome/free-solid-svg-icons';
+
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const ForecastDetail = ({ data }) => {
   console.log(data);
-  var main = data[0].main;
-  var d = new Date(data[0].dt_txt);
-  var dayname = weekday[d.getDay()];
+  const main = data[0].main;
+  const d = new Date(data[0].dt_txt);
+  const dayname = weekday[d.getDay()];
+  let iconClassn;
+
   if (data[0].weather[0].main === "Rain") {
-    var iconClassn = faCloudShowersHeavy;
+    iconClassn = faCloudShowersHeavy;
   } else if (data[0].weather[0].main === "Clouds") {
-    var iconClassn = faCloud
+    iconClassn = faCloud;
   } else if (data[0].weather[0].main === "Clear") {
-    var iconClassn = faSun
+    iconClassn = faSun;
   }
+
   return (
     <>
-      <div class="cards" id="weather-cards"><div class="card">
-        <h2 class="day-name">{dayname}</h2>
-        <div class="card-icon">
-
-          <FontAwesomeIcon icon={iconClassn} />
-
+      <div className="cards" id="weather-cards">
+        <div className="card">
+          <h2 className="day-name">{dayname}</h2>
+          <div className="card-icon">
+            <FontAwesomeIcon icon={iconClassn} />
+          </div>
+          <div className="day-temp">
+            <h2 className="temp">{toCel(main.feels_like - 204.8).toFixed(2)}</h2>
+            <span className="temp-unit">°C</span>
+          </div>
         </div>
-        <div class="day-temp">
-          <h2 class="temp">{toCel(main.feels_like - 204.8).toFixed(2)}</h2>
-          <span class="temp-unit">°C</span>
-        </div>
-      </div></div>
+      </div>
     </>
   );
-
-
 }
 
 function toCel(temp) {
   return (temp - 32) / 1.8;
 }
 
-
-export default ForecastDetail
+export default ForecastDetail;
